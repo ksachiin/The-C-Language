26a538e7366e1a907764bce61c6fa3143470ab64
ksachiin <7285sachinkareliya@gmail.com>
2024-07-19 21:26:50 +0530

    Add files via upload

---
+++ Array Exam/Q-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main()
+{
+    int size, i, j;
+    printf("Enter Arry Size :- ");
+    scanf("%d", &size);
+
+    int a[size];
+    for (i = 0; i < size; i++)
+    {
+        printf("enter [%d]", i);
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = i + 1; j < size; j++)
+        {
+            if (a[i] == a[j])
+            {
+                printf("%d", a[i]);
+            }
+        }
+        printf("\n");
+    }
+}
